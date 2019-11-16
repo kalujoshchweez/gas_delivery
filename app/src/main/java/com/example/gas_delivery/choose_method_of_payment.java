b67cae6488b2be15fb1be502d67c11376f13c3e4
chweezy <ngugichweya@gmail.com>
2019-11-16 18:15:32 +0300

    mpesa api intergration part 1 of 4 complete

---
+++ app/src/main/java/com/example/gas_delivery/choose_method_of_payment.java
@@ -0,0 +1,14 @@
+package com.example.gas_delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class choose_method_of_payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_method_of_payment);
+    }
+}
---
+++ app/src/main/java/com/example/gas_delivery/mpesa_payment.java
@@ -0,0 +1,14 @@
+package com.example.gas_delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mpesa_payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mpesa_payment);
+    }
+}
